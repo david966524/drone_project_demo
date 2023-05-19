51a289aec9a07dc36794306ed0e2b8019a5dbaa9
david966524 <david9665241926@gmail.com>
2023-05-19 14:46:50 +0700

    update

--- drone-ci-golang/main.go
+++ drone-ci-golang/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
-  "github.com/sirupsen/logrus"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-  r := gin.Default()
+	r := gin.Default()
 
-  r.GET("/health", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H {
-      "health": true,
-    })
-  })
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"health": true,
+		})
+	})
 
-  if err := r.Run(":8080"); err != nil {
-    logrus.WithError(err).Fatal("Couldn't listen")
-  }
+	if err := r.Run(":8080"); err != nil {
+		logrus.WithError(err).Fatal("Couldn't listen")
+	}
 
 }
